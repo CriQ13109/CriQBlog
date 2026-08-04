b578f53d504e49d9ab0de923bf60c503d8b72a03
CriQ <criq13109@gmail.com>
2026-08-04 08:41:41 +0000

    Updated 1 file via CloudCannon.

--- src/lib/createSlug.ts
+++ src/lib/createSlug.ts
@@ -11,7 +11,7 @@ export default function (title: string, staticSlug: string) {
       .toLowerCase()
       // replace spaces
       .replace(/\s+/g, '-')
-      // remove special characters (한글 범위 가-힣 추가)
+      // remove special characters
       .replace(/[^\w-가-힣]/g, '')
       // remove leading & trailing separtors
       .replace(/^-+|-+$/g, '')
