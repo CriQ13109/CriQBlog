7f375b3241b67e67af126fbb9df61ec88a05bc8b
CriQ <criq13109@gmail.com>
2026-08-05 05:57:32 +0000

    Updated 1 file via CloudCannon.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.criqtech.com/',
   integrations: [mdx(), sitemap(), tailwind()]
+  redirects: {
+    '/cv': '/cv/criq',
+  },
 });
