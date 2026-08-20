ec2e56ab61c1bdac785aa1b7b7c6f485c8ff7754
CriQ <criq13109@naver.com>
2026-08-20 11:02:25 +0900

    reerror

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
 import { z, defineCollection } from "astro:content";
 
-const blogSchema = z.object({
+const blogSchema = ({ image }: { image: any }) =>
+  z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
@@ -8,33 +9,36 @@ const blogSchema = z.object({
     heroImage: image().optional(),
     badge: z.string().optional(),
     category: z.union([z.string(), z.array(z.string())]).optional(),
-    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
-        message: 'tags must be unique',
-    }).optional(),
+    tags: z
+      .array(z.string())
+      .refine((items) => new Set(items).size === items.length, {
+        message: "tags must be unique",
+      })
+      .optional(),
     author: z.string().default("CriQ"),
-});
+  });
 
 const storeSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    custom_link_label: z.string(),
-    custom_link: z.string().optional(),
-    updatedDate: z.coerce.date(),
-    pricing: z.string().optional(),
-    oldPricing: z.string().optional(),
-    badge: z.string().optional(),
-    checkoutUrl: z.string().optional(),
-    heroImage: z.string().optional(),
-    category: z.string().optional(),
+  title: z.string(),
+  description: z.string(),
+  custom_link_label: z.string(),
+  custom_link: z.string().optional(),
+  updatedDate: z.coerce.date(),
+  pricing: z.string().optional(),
+  oldPricing: z.string().optional(),
+  badge: z.string().optional(),
+  checkoutUrl: z.string().optional(),
+  heroImage: z.string().optional(),
+  category: z.string().optional(),
 });
 
-export type BlogSchema = z.infer<typeof blogSchema>;
+export type BlogSchema = z.infer<ReturnType<typeof blogSchema>>;
 export type StoreSchema = z.infer<typeof storeSchema>;
 
 const blogCollection = defineCollection({ schema: blogSchema });
 const storeCollection = defineCollection({ schema: storeSchema });
 
 export const collections = {
-    'blog': blogCollection,
-    'store': storeCollection
-}
+  blog: blogCollection,
+  store: storeCollection,
+};
